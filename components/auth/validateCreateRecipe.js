db8fa27b32f8baf60c01d86caa599f9a38b31f73
Tristan Gruener <tristan.gruener@gmail.com>
2020-11-05 20:58:59 -0800

    create recipe page and form basic ssetup

---
+++ components/auth/validateCreateRecipe.js
@@ -0,0 +1,14 @@
+export default function validateCreateRecipe(values) {
+  let errors = {}
+
+  // email errors
+  if (!values.name) {
+    errors.name = "Recipe Name is Required"
+  }
+  // password errors
+  if (!values.steps) {
+    errors.steps = "Steps are required... how do you make a recipe without them?"
+  }
+
+  return errors
+}
--- pages/create-recipe.js
+++ pages/create-recipe.js
@@ -1,25 +1,57 @@
 import Layout from '../components/layout'
+import useFormValidation from '../components/auth/useFormValidation'
+import validateCreateRecipe from '../components/auth/validateCreateRecipe'
 
-export default function CreateRecipe() {
+const INITIAL_STATE = {
+  name: "",
+  steps: ""
+}
+
+export default function CreateRecipe(props) {
+  const { handleSubmit, handleChange, values, errors } = useFormValidation(INITIAL_STATE, validateCreateRecipe, handleCreateRecipe)
+
+  function handleCreateRecipe() {
+    console.log('recipe created')
+  }
+
   return (
     <Layout>
       <div className="columns">
         <div className="column is-three-fifths">
+
           <div className="content">
             <h3>create recipe page</h3>
           </div>
-          <input
-            type="text"
-            placeholder="Recipe Name"
-            className="input"
-          />
 
-          <div>
-            <button className="button">Add Recipe</button>
-          </div>
-          <br />
-          {/* {isPasswordReset && <p className="has-text-success">Check email to reset password 👍</p>}
-          {passwordResetError && <p className="has-text-danger">{passwordResetError}</p>} */}
+          <form onSubmit={handleSubmit}>
+
+            <input
+              onChange={handleChange}
+              value={values.name}
+              name="name"
+              type="text"
+              placeholder="Recipe Name"
+              className="input"
+              autoComplete="off"
+            />
+            {errors.name && <p className="has-text-danger">{errors.name}</p>}
+
+            <input
+              onChange={handleChange}
+              value={values.steps}
+              name="steps"
+              type="text"
+              placeholder="Recipe Steps"
+              className="input"
+              autoComplete="off"
+            />
+            {errors.steps && <p className="has-text-danger">{errors.steps}</p>}
+
+            <div>
+              <button className="button">Add Recipe</button>
+            </div>
+          </form>
+
         </div>
       </div>
     </Layout>
