cd80c0218fef70f393c22ac962e882018b4caa4c
Tristan Gruener <tristan.gruener@gmail.com>
2021-04-30 12:49:16 -0700

    save

--- pages/meal-planner.js
+++ pages/meal-planner.js
@@ -1,52 +1,38 @@
 import Layout from '../components/layout'
 
+const WEEKDAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
 export default function MealPlanner() {
 
   const today = new Date();
-  var currentMonth = today.getMonth() + 1
+  const day = today.getDay()
 
-  const [ dayOfWeekInt, setDayOfWeek ] = React.useState(today.getDay())
+  const currentMonth = today.getMonth() + 1
+
+  const [ dayOfWeekInt, setDayOfWeek ] = React.useState(day)
 
   let currentDay = ''
 
-  switch(today.getDay()) {
-      case 0:
-        currentDay = 'Sunday'
-        break;
-      case 1:
-        currentDay = 'Monday'
-        break;
-      case 2:
-        currentDay = 'Tuesday'
-        break;
-      case 3:
-        currentDay = 'Wednesday'
-        break;
-      case 4:
-        currentDay = 'Thursday'
-        break;
-      case 5:
-        currentDay = 'Friday'
-        break;
-      case 6:
-        currentDay = 'Saturday'
-        break;
+  for (let i in WEEKDAYS) {
+    // alert(day)
+    if (i == day) {
+      currentDay = WEEKDAYS[i]
+      console.log(i)
+    }
   }
 
-  const [ dayOfWeekText, setDayOfWeekText ] = React.useState(currentDay + currentMonth + ' / ' + String(today.getDate()).padStart(2, '0'))
+  const [ dayOfWeekText, setDayOfWeekText ] = React.useState(currentDay + ' ' + currentMonth + ' / ' + String(today.getDate()).padStart(2, '0'))
 
-  const startWeekIndex = today.getDay() * -1
+  React.useEffect(() => {
+    const startWeekIndex = today.getDay() * -1
 
-
-
-  for (let i = startWeekIndex; i <= startWeekIndex + 6; i++) {
-    console.log(i)
-
-    var currentDate = new Date()
-    var newDate = currentDate.setDate(currentDate.getDate() + i)
-    console.log(new Date(newDate))
-  }
-  console.log('---')
+    for (let i = startWeekIndex; i <= startWeekIndex + 6; i++) {
+      console.log(i)
+      var currentDate = new Date()
+      var newDate = currentDate.setDate(currentDate.getDate() + i)
+      console.log(new Date(newDate))
+    }
+  },[dayOfWeekInt])
 
   return (
     <Layout>
@@ -64,19 +50,13 @@ export default function MealPlanner() {
           <table className="table is-bordered">
             <tbody>
               <tr>
-                <td className={dayOfWeekInt === 0 && "has-background-info-light is-active"}>Sunday</td>
-                <td className={dayOfWeekInt === 1 && "has-background-info-light is-active"}>Monday</td>
-                <td className={dayOfWeekInt === 2 && "has-background-info-light is-active"}>Tuesday</td>
-                <td className={dayOfWeekInt === 3 && "has-background-info-light is-active"}>Wednesday</td>
-                <td className={dayOfWeekInt === 4 && "has-background-info-light is-active"}>Thursday</td>
-                <td className={dayOfWeekInt === 5 && "has-background-info-light is-active"}>Friday</td>
-                <td className={dayOfWeekInt === 6 && "has-background-info-light is-active"}>Saturday</td>
+                {WEEKDAYS.map((day, i) => (
+                  <td className={dayOfWeekInt === i && "has-background-info-light is-active"}>{WEEKDAYS[i]}</td>
+                ))}
               </tr>
             </tbody>
           </table>
         </div>
-
-
 
         <div className="day-view">
           <h4>{dayOfWeekText}</h4>
