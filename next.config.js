aa98118682f4781badc4b9989ab262e8e7a72a81
Tristan Gruener <tristan.gruener@gmail.com>
2021-10-31 22:45:21 -0700

    cache control test

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/:slug/index',
+        source: '/:slug/*',
         headers: [
           {
             key: 'Cache-Control',
-            value: 'no-cache, no-store, max-age=0, must-revalidate',
-          },
+            value: 'public, max-age=9999999999, must-revalidate',
+          }
         ],
       },
     ]
