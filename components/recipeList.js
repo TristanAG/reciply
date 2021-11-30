d0359c7a5b20f5cba20c1f08228d19e0fe2bcb27
Tristan Gruener <tristan.gruener@gmail.com>
2021-11-30 12:38:47 -0800

    working at a basic level now

--- components/recipeList.js
+++ components/recipeList.js
@@ -26,22 +26,15 @@ export default function RecipeList(props) {
   }
 
   function updateTags(tag) {
-
-    // var newArr = tags
-    // newArr.push(tag)
-    // setProto(newArr)
-    
-
     setTags([...tags, tag])
     updateRecipeList([...tags, tag])
-
   }
 
   function updateRecipeList(tags) {
-    console.log(tags)
+    console.log(tags.length)
     // const recipesWithTags = firebase.db.collection('recipes').where("tags", "array-contains-any", tags).orderBy('created', 'desc').onSnapshot(handleSnapshot)
     // firebase.db.collection('recipes').where("tags", "array-contains-any", tags).orderBy('created', 'desc').onSnapshot(handleSnapshot)
-    console.log('🚨 🚨 🚨 pause query 🚨 🚨 🚨')
+
     // firebase.db.collection('recipes').where("tags", "array-contains-any", tags).get().then((querySnapshot) => {
     //   querySnapshot.forEach((doc) => {
     //     console.log(doc.data())
@@ -50,6 +43,41 @@ export default function RecipeList(props) {
 
 
     //maybe i don't need to use the snapshot... that's an idea
+
+    if (tags.length === 1) {
+      firebase.db.collection('recipes').where("tags", "array-contains", tags[0]).orderBy('created', 'desc').onSnapshot(handleSnapshot)
+    } else {
+      console.log('🚨 🚨 🚨 pause query 🚨 🚨 🚨')
+
+      //so here i will do the array logic, is that true? or perhapse, i will do the querySnapshot method here?
+      //actually, i can manipulate the recipes because i already have that object available... i don't know if this is the right way, but again , mvp...
+
+      // console.log(recipes)
+
+      // so loop through the recipes
+      let filteredRecipes = []
+      recipes.forEach(recipe => {
+        if (recipe.tags.includes(tags[tags.length - 1])) {
+          console.log('hit')
+          console.log(recipe)
+          filteredRecipes.push(recipe)
+        }
+      })
+
+      console.log('filtered recipes')
+      console.log(filteredRecipes)
+
+      setRecipes(filteredRecipes)
+
+      // if (recipes.length >= 1) {
+      //   setRecipes(recipes)
+      // }
+
+
+      // console.log('last element')
+      // console.log(tags[tags.length - 1])
+    }
+
 
     window.scrollTo(0,0)
   }
