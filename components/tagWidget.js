fe74af31fb551ab76205a4e80311d23cb8c1dfc9
Tristan Gruener <tristan.gruener@gmail.com>
2021-12-03 11:10:16 -0800

    tagwidget style

--- components/tagWidget.js
+++ components/tagWidget.js
@@ -11,22 +11,26 @@ function tagWidget({ tags, handleRemoveTag }) {
 
   return(
     <div className="tag-widget">
-      <div style={{"width": "220px"}}><input className="input is-primary" type="text" placeholder="Recipe, Cuisine, Ingredient" style={{"maxWidth":"100%"}}/></div>
-      <span>
-        <div className="tags are-medium has-addons">
-          {activeTags && activeTags.map(tag => (
-            <React.Fragment key={tag}>
-              <span className="tag has-background-link-light">{tag}</span>
-              <a className="tag is-delete" onClick={() => handleRemoveTag(tag)}></a>&nbsp;&nbsp;
-            </React.Fragment>
-          ))}
-        </div>
-      </span>
+      <div className="text-input">
+        <input className="input is-primary" type="text" placeholder="Add Tag, Recipe Name, Type of Cuisine or Ingredient" style={{"maxWidth":"100%"}}/>
+      </div>
+
+      <div className="tags are-medium has-addons">
+        {activeTags && activeTags.map(tag => (
+          <React.Fragment key={tag}>
+            <span className="tag has-background-link-light">{tag}</span>
+            <a className="tag is-delete" onClick={() => handleRemoveTag(tag)}></a>&nbsp;&nbsp;
+          </React.Fragment>
+        ))}
+      </div>
 
       <style jsx>{`
 
         .tag-widget {
 
+          width: 464px;
+          margin-left: auto;
+          margin-right: auto;
         }
       `}</style>
     </div>
